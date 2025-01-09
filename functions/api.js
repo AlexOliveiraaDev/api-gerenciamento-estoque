9a1df3bc5a4a1d1a201dbd77e2694d9fccb085a2
Alex Oliveira <157441640+AlexOliveiraaDev@users.noreply.github.com>
2025-01-08 22:07:25 -0300

    fix: fixed db connection

--- functions/api.js
+++ functions/api.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Carregar as variáveis de ambiente do arquivo .env
+require('dotenv').config(); 
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
@@ -9,11 +9,9 @@ const app = express();
 const port = 3000;
 const router = express.Router();
 
-// Middleware para parsear o corpo das requisições
 app.use(express.json());
 app.use(cors());
 
-// Configurações do banco de dados usando variáveis de ambiente
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -22,7 +20,6 @@ const db = mysql.createConnection({
     database: process.env.DB_DATABASE
 });
 
-// Conectar ao banco de dados
 db.connect((err) => {
     if (err) {
         console.error('Erro ao conectar ao banco de dados: ', err);
@@ -31,7 +28,6 @@ db.connect((err) => {
     console.log('Conectado ao banco de dados MySQL');
 });
 
-// 1. Ler todos os itens da tabela produtos
 router.get('/produtos', (req, res) => {
     db.query('SELECT * FROM produtos_db', (err, results) => {
         if (err) {
@@ -41,7 +37,6 @@ router.get('/produtos', (req, res) => {
     });
 });
 
-// 2. Inserir um novo item na tabela produtos
 router.post('/produtos', (req, res) => {
     const { produto, categoria, quantidade, preco, localizacao } = req.body;
     const query = 'INSERT INTO produtos_db (produto, categoria, quantidade, preco, localizacao) VALUES (?, ?, ?, ?, ?)';
@@ -54,7 +49,6 @@ router.post('/produtos', (req, res) => {
     });
 });
 
-// 3. Apagar um item da tabela produtos
 router.delete('/produtos/:id', (req, res) => {
     const { id } = req.params;
     db.query('DELETE FROM produtos_db WHERE id = ?', [id], (err, results) => {
@@ -65,7 +59,6 @@ router.delete('/produtos/:id', (req, res) => {
     });
 });
 
-// 4. Alterar um item da tabela produtos
 router.put('/produtos/:id', (req, res) => {
     const { id } = req.params;
     const { produto, categoria, quantidade, preco, localizacao } = req.body;
@@ -78,11 +71,6 @@ router.put('/produtos/:id', (req, res) => {
         res.json({ message: 'Produto atualizado com sucesso' });
     });
 });
-
-// Iniciar o servidor
-// app.listen(port, () => {
-//     console.log(`API rodando em http://localhost:${port}`);
-// });
 
 app.use('/api',router)
 module.exports.handler = serverless(app)
